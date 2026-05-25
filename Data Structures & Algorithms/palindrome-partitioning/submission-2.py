9a47acd8f97044bfaffb265bf5c0f15f38f0dda1
rodrigo (roger) <126273948+rodrigomacedo-sjr@users.noreply.github.com>
2026-05-25 07:50:57 -0300

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,22 @@
+from functools import cache
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        SIZE = len(s)
+
+        @cache
+        def dfs(i):
+            if i == SIZE:
+                return [[]]
+            ans = []
+            for j in range(i, SIZE):
+                sub = s[i : j + 1]
+                bus = sub[::-1]
+                if sub == bus:
+                    ways = dfs(j + 1)
+                    for way in ways:
+                        ans.append([sub] + way)
+            return ans
+                    
+
+        return dfs(0)
