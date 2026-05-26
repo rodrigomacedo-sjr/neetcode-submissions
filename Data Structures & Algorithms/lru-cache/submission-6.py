c55af7f54a7ed9bb84ef6c400218eb06af7f1e93
rodrigo (roger) <126273948+rodrigomacedo-sjr@users.noreply.github.com>
2026-05-26 20:45:21 -0300

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.py
@@ -0,0 +1,52 @@
+class Node:
+    __slots__ = ('val', 'key', 'pv', 'nx')
+    def __init__(self, val, key, pv = None, nx = None):
+        self.val = val
+        self.key = key
+        self.pv = pv
+        self.nx = nx
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0, self.head)
+        self.head.nx = self.tail
+
+        self.map = {}
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.map:
+            return -1
+        node = self.map[key]
+
+        self._remove(node)
+        self._add(node)
+        return node.val
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            node = self.map[key]
+            node.val = value
+            self._remove(node)
+        else:
+            node = Node(value, key)
+            self.map[key] = node
+        self._add(node)
+
+        if len(self.map) > self.capacity:
+            del self.map[self.tail.pv.key]
+            self._remove(self.tail.pv)
+        
+    def _remove(self, node):
+        node.pv.nx = node.nx
+        node.nx.pv = node.pv
+    
+    def _add(self, node):
+        node.nx = self.head.nx
+        node.pv = self.head
+
+        self.head.nx.pv = node
+        self.head.nx = node
+
